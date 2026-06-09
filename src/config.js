c0d1d9a2b9d522b59b2c47ef7c43e51afd8bfd8e
jwhicks727 <jwhicks727@gmail.com>
2026-06-09 10:15:13 -0700

    Disable suspense mode, enable summer mode after Tournament of Houses reveal

--- src/config.js
+++ src/config.js
@@ -4,5 +4,5 @@
 // SUSPENSE_MODE: true = hide all point totals
 // Flip to false after the tournament reveal, then deploy.
 
-export const SUSPENSE_MODE = true  // flip to false after reveal
-export const SUMMER_MODE = false   // flip to true after reveal for summer
+export const SUSPENSE_MODE = false  // flip to false after reveal
+export const SUMMER_MODE = true   // flip to true after reveal for summer
